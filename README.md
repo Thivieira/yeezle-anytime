a9f60ce3648d31e98a58e2c8717fb4e9b8aed83c
Thivieira <thiago.a.vieira@hotmail.com>
2022-08-08 21:59:07 -0300

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# Yeezle Anytime
+
+This repository contains the source code of Yeezle Anytime script.
+
+## Installation (Chrome, Firefox, Safari, Edge, Opera)
+
+1. Install [Tampermonkey](https://www.tampermonkey.net/)
+2. Press [here](https://github.com/Thivieira/yeezle-anytime/raw/main/anytime.js), a Tampermonkey tab should open
+3. Press install, and you're done
+
+## License
+
+This project is MIT Licensed
---
+++ anytime.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Yeezle Anytime
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  I just wished this game was fun to play anytime, not just one game per day!
+// @author       Thiago Vieira
+// @match        https://yeezle.xyz/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=yeezle.xyz
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var mysteryNumber = {};
+    var mysteryDouble = {};
+    var mysterySong = {};
+    async function getRandomMysterySong() {
+    mysteryNumber = Math.floor(Math.random() * 175) + 1;
+    await fetch('/datasheetNoSkit.json')
+        .then(response => response.json())
+        .then(data => {
+            mysteryDouble = data.numbers[mysteryNumber].title
+        })
+    await fetch('/datasheetNoSkit.json')
+        .then(response => response.json())
+        .then(data => {
+            mysterySong = data.songs[mysteryDouble]
+        })
+    }
+    getRandomMysterySong();
+})();
